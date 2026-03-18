cbdc5178062710b2faef1a51f03ed22cbfd5755e
wildanfajar <wildanfajarpamungkas964@gmail.com>
2026-03-18 20:41:35 +0700

    menambah animasi dan toogle menu

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+btn.addEventListener("click", function () {
+  navLinks.classList.toggle("show-menu");
+  
+});
